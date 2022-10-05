a5876bc0d35489711238f32b208b57b951043057
Matheus Bernardes Spilari <60487141+MSpilari@users.noreply.github.com>
2022-10-05 17:21:33 -0300

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return <h1 className="bg-blue-500">Hello World</h1>
+}
+
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/*.jsx',
+    './src/pages/*.tsx',
+    './src/pages/*.ts',
+    './src/components/**/*.ts',
+    './src/components/**/*.jsx',
+    './src/components/**/*.tsx'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
