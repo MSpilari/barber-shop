ec23b90afe8d7a5195350af6c7f69eced6c54719
MSpilari <mb.spilari@hotmail.com>
2022-10-08 14:10:19 -0300

    Inserindo os componentes welcome e header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
 import type { NextPage } from 'next'
+import { Header } from '../components/Header'
+import { Welcome } from '../components/Welcome'
 
 const Home: NextPage = () => {
-  return <h1 className="bg-blue-500">Hello World</h1>
+  return (
+    <main className="w-screen h-screen flex flex-col items-center justify-center">
+      <Welcome />
+      <Header />
+    </main>
+  )
 }
 
 export default Home
