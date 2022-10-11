0456a79b2a8bad56b199b4388013b85c854f7979
MSpilari <mb.spilari@hotmail.com>
2022-10-11 13:18:05 -0300

    Inserindo o HomeBg agora como componente

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from 'next'
 import { Header } from '../components/Header'
+import { HomeBg } from '../components/HomeBg'
 import { Welcome } from '../components/Welcome'
 
 const Home: NextPage = () => {
   return (
     <main className="w-screen h-screen flex flex-col items-center justify-center">
-      <Welcome />
+      <HomeBg />
       <Header />
+      <Welcome />
     </main>
   )
 }
