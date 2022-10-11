47417a9a95b072b85cb4f858ab80b2e70be4d968
MSpilari <mb.spilari@hotmail.com>
2022-10-11 13:17:33 -0300

    Retirando o bg e alterações de CSS

--- src/components/Welcome/index.tsx
+++ src/components/Welcome/index.tsx
@@ -1,24 +1,22 @@
 const Welcome = () => {
   return (
-    <div className="bg-home bg-no-repeat bg-cover w-full h-[100vh] flex flex-col items-center justify-center">
-      <div className="flex flex-col items-center gap-12 mt-24 w-80">
-        <span className="text-white font-normal font-Roboto text-center text-xs">
-          Since 1989
-        </span>
+    <div className="flex flex-col items-center gap-12 mt-24 w-80 lg:mt-40 lg:w-[31rem]">
+      <span className="text-white font-normal font-Roboto text-center text-xs lg:text-base">
+        Since 1989
+      </span>
 
-        <span className="text-white font-normal text-center text-3xl uppercase">
-          The <span className="text-orange-400">Man`s</span> Lounge
-        </span>
+      <span className="text-white font-normal text-center text-3xl uppercase lg:text-6xl">
+        The <span className="text-orange-400">Man`s</span> Lounge
+      </span>
 
-        <span className="text-white font-normal font-Roboto text-center text-base">
-          A desire to trim your beard, refresh your haircut and get pampered?
-          This is the ideal place.
-        </span>
+      <span className="text-white font-normal font-Roboto text-center text-xs lg:text-base">
+        A desire to trim your beard, refresh your haircut and get pampered? This
+        is the ideal place.
+      </span>
 
-        <button className="w-52 h-20 border rounded-full text-white">
-          See everything
-        </button>
-      </div>
+      <button className="w-52 h-20 border rounded-full text-white hover:text-orange-400 hover:border-orange-400">
+        See everything
+      </button>
     </div>
   )
 }
