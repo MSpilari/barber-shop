6f643b8f23a79b10459b57c3f4906f952b913d45
MSpilari <mb.spilari@hotmail.com>
2022-10-11 17:48:29 -0300

    Criando o primeiro Service

---
+++ src/components/Services/index.tsx
@@ -0,0 +1,19 @@
+const Services = () => {
+  return (
+    <section className="w-full flex flex-col">
+      <div className="bg-orange-400 h-20">Shave & Cut</div>
+      <span>The Beard</span>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis
+        pariatur qui laudantium perspiciatis amet temporibus expedita, nemo
+        corporis atque harum autem incidunt repudiandae doloremque tenetur.
+        Corporis sit sunt recusandae quidem.
+      </p>
+      <button>Find out more.</button>
+      <span>Pic 1</span>
+      <span>Pic 2</span>
+    </section>
+  )
+}
+
+export { Services }
