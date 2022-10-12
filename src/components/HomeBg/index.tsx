79871d9ceac7b835f16178b9b5001e84e4a43dbe
MSpilari <mb.spilari@hotmail.com>
2022-10-12 18:06:23 -0300

    Aumentando o tamanho do bg

--- src/components/HomeBg/index.tsx
+++ src/components/HomeBg/index.tsx
@@ -1,5 +1,5 @@
 const HomeBg = () => (
-  <div className="bg-home bg-no-repeat bg-cover w-full h-[100vh] absolute top-0 -z-10" />
+  <div className="bg-home bg-no-repeat bg-cover w-full h-[105vh] lg:h-[110vh] absolute top-0 -z-10" />
 )
 
 export { HomeBg }
