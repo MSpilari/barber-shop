27a8449543a6b9b2c1ba5bb704c189e1d1009f6d
MSpilari <mb.spilari@hotmail.com>
2022-10-13 13:02:26 -0300

    Finalizando a estilização do texto

--- src/components/Services/index.tsx
+++ src/components/Services/index.tsx
@@ -29,19 +29,24 @@ const Services = () => {
           />
         </div>
 
-        <div className="lg:w-96 lg:h-96">
-          <span className="text-3xl">
+        <div className="w-80 h-80 flex flex-col items-center gap-8 lg:w-96 lg:h-96 lg:gap-12 xl:items-end">
+          <span className="text-3xl uppercase lg:text-6xl">
             The <span className="text-orange-400">Beard</span>
           </span>
 
-          <p>
+          <p className="text-sm uppercase text-center w-80 h-40 font-Roboto lg:text-base xl:text-right">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis
             pariatur qui laudantium perspiciatis amet temporibus expedita, nemo
             corporis atque harum autem incidunt repudiandae doloremque tenetur.
             Corporis sit sunt recusandae quidem.
           </p>
 
-          <button>Find out more.</button>
+          <button
+            className="w-52 h-20 border border-black rounded-full 
+          hover:text-orange-400 hover:border-orange-400"
+          >
+            Find out more
+          </button>
         </div>
       </div>
     </section>
