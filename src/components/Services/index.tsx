95fcf3aed2fcaf171fdfe81745016b7d91c027c9
MSpilari <mb.spilari@hotmail.com>
2022-10-13 12:36:27 -0300

    Finalizando a estilização das imagens

--- src/components/Services/index.tsx
+++ src/components/Services/index.tsx
@@ -12,33 +12,37 @@ const Services = () => {
         Shave & Cut
       </div>
 
-      <div className="z-10 flex flex-col items-center justify-center">
-        <Image
-          src={barber_1}
-          className="w-40 h-40"
-          alt="barber_1 Pic"
-          sizes="50vw"
-        />
+      <div className="z-10 flex flex-col items-center justify-center lg:flex-row-reverse">
+        <div className="relative w-80 h-80 flex lg:w-1/2 lg:h-[50rem]">
+          <Image
+            src={barber_1}
+            className="w-40 h-40 absolute z-10 left-0 top-24 lg:w-80 lg:h-80 lg:-left-40 xl:left-0 lg:top-64"
+            alt="barber_1 Pic"
+            sizes="50vw"
+          />
 
-        <Image
-          src={barber_2}
-          className="w-60"
-          alt="barber_2 Pic"
-          sizes="50vw"
-        />
+          <Image
+            src={barber_2}
+            className="w-60 absolute right-0 -top-5 lg:w-[33.75rem] lg:-top-7"
+            alt="barber_2 Pic"
+            sizes="50vw"
+          />
+        </div>
 
-        <span className="text-3xl">
-          The <span className="text-orange-400">Beard</span>
-        </span>
+        <div className="lg:w-96 lg:h-96">
+          <span className="text-3xl">
+            The <span className="text-orange-400">Beard</span>
+          </span>
 
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis
-          pariatur qui laudantium perspiciatis amet temporibus expedita, nemo
-          corporis atque harum autem incidunt repudiandae doloremque tenetur.
-          Corporis sit sunt recusandae quidem.
-        </p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis
+            pariatur qui laudantium perspiciatis amet temporibus expedita, nemo
+            corporis atque harum autem incidunt repudiandae doloremque tenetur.
+            Corporis sit sunt recusandae quidem.
+          </p>
 
-        <button>Find out more.</button>
+          <button>Find out more.</button>
+        </div>
       </div>
     </section>
   )
