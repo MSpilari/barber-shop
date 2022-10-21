8a70f8c0d09212c2f9607c2c5939cada80504a48
MSpilari <mb.spilari@hotmail.com>
2022-10-21 17:57:52 -0300

    Separação do componente visando melhor entendimento

---
+++ src/components/Contact/AddressPhoneEmail.tsx
@@ -0,0 +1,19 @@
+const AddressPhoneEmail = () => {
+  return (
+    <div className="w-80 h-36 flex flex-col items-center gap-6">
+      <p className="text-lg font-Roboto text-center lg:text-2xl">
+        7813 Cherry Hill Ave. <br /> Snohomish, Paris 98290
+      </p>
+
+      <p className="text-lg font-Roboto text-center lg:text-2xl">
+        📞 +00 00 00 00 00 00
+      </p>
+
+      <p className="text-lg font-Roboto text-center lg:text-2xl">
+        👋 hello@frenchbarber.fr
+      </p>
+    </div>
+  )
+}
+
+export { AddressPhoneEmail }
