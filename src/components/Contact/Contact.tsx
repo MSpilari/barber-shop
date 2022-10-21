e2c3149f7caff50073f92e9eabd1d0cefb77caa8
MSpilari <mb.spilari@hotmail.com>
2022-10-21 17:59:31 -0300

    Inserindo classes p/ screen large e componentes de email/mediaIcons

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,76 +1,41 @@
 import Image from 'next/future/image'
 import contact_1 from '../../assets/contact_1.png'
-import {
-  FaFacebookF,
-  FaTwitter,
-  FaInstagram,
-  FaLinkedinIn
-} from 'react-icons/fa'
+import { SectionBadge } from '../Services/Common/SectionBadge'
+import { AddressPhoneEmail } from './AddressPhoneEmail'
+import { SocialMediaIcons } from './SocialMediaIcons'
 
 const Contact = () => {
-  const iconsList = [
-    <FaFacebookF key="FaFacebookF" />,
-    <FaTwitter key="FaTwitter" />,
-    <FaInstagram key="FaInstagram" />,
-    <FaLinkedinIn key="FaLinkedinIn" />
-  ]
-
   return (
     <section className="w-full flex flex-col overflow-x-hidden">
-      <div
-        className="bg-orange-400 h-20 text-5xl mt-4 uppercase flex items-center justify-center whitespace-nowrap rotate-2 
-         lg:text-8xl lg:h-24 lg:mt-6"
-      >
-        Contact Us
-      </div>
+      <SectionBadge title="Contact Us" extraStyleDiv="bg-orange-400 rotate-2" />
 
       <div className="z-10 flex flex-col items-center justify-center lg:flex-row">
         <div className="relative w-80 h-80 flex lg:w-1/2 lg:h-[50rem]">
           <Image
             src={contact_1}
-            className="w-64 h-80 absolute z-10 left-0 -top-6 lg:left-14 lg:w-[34rem] lg:h-[35rem] xl:h-[41rem] "
+            className="w-64 h-80 absolute z-10 left-0 -top-6 xl:left-14 lg:w-[34rem] lg:h-[35rem] xl:h-[41rem] "
             alt="barber_1 Pic"
             sizes="50vw"
           />
         </div>
 
-        <div className="w-80 h-[26rem] flex flex-col items-center gap-12 lg:w-[38rem] lg:h-[31rem]">
-          <div className="w-80 h-72 flex flex-col items-center gap-10">
-            <div className="w-80 h-28 flex flex-col items-center gap-5">
-              <h1 className="uppercase text-2xl text-center">
+        <div className="w-80 h-[26rem] flex flex-col items-center gap-12 lg:self-start lg:mt-20 xl:w-[38rem] xl:h-[31rem]">
+          <div className="w-80 h-72 flex flex-col items-center gap-10 xl:w-[38rem] xl:h-[25rem]">
+            <div className="w-80 h-28 flex flex-col items-center gap-5 xl:w-[38rem] xl:h-40">
+              <h1 className="uppercase text-2xl text-center lg:text-2xl xl:text-5xl">
                 Contact Information
               </h1>
 
-              <p className="text-sm font-Roboto text-center">
+              <p className="text-sm font-Roboto text-center lg:text-lg">
                 Prototypes may also be exempted from some requirements that will
                 apply to the final product
               </p>
             </div>
 
-            <div className="w-80 h-36 flex flex-col items-center gap-6">
-              <p className="text-lg font-Roboto text-center">
-                7813 Cherry Hill Ave. <br /> Snohomish, Paris 98290
-              </p>
-
-              <p className="text-lg font-Roboto text-center">
-                📞 +00 00 00 00 00 00
-              </p>
-
-              <p className="text-lg font-Roboto text-center">
-                👋 hello@frenchbarber.fr
-              </p>
-            </div>
+            <AddressPhoneEmail />
           </div>
-          <ul className="w-80 h-14 flex items-center justify-center gap-6">
-            {iconsList.map((icon, index) => (
-              <li
-                key={index}
-                className="w-14 h-14 flex items-center justify-center text-xl border border-solid border-black rounded-full"
-              >
-                {icon}
-              </li>
-            ))}
-          </ul>
+
+          <SocialMediaIcons />
         </div>
       </div>
     </section>
