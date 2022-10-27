6e9194ddf5ac8f0ed240c7ed671be4fce2049b5f
MSpilari <mb.spilari@hotmail.com>
2022-10-27 16:50:10 -0300

    Adicionando o bg com formato png

--- src/components/HomeBg/index.tsx
+++ src/components/HomeBg/index.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/future/image'
-import bg_home from '../../assets/bg_home.svg'
+import bg_home from '../../assets/bg_home.png'
 
 const HomeBg = () => (
   <div className="w-full h-[105vh] lg:h-[110vh] absolute top-0 -z-10">
@@ -8,7 +8,7 @@ const HomeBg = () => (
       className="w-full h-full object-cover"
       alt="bg_home"
       sizes="33vw"
-      quality={25}
+      quality={100}
     />
   </div>
 )
