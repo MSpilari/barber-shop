5728226d51a862037804193fbe638e6457896fce
MSpilari <mb.spilari@hotmail.com>
2022-10-27 12:50:34 -0300

    BgHome se torna component next/image

--- src/components/HomeBg/index.tsx
+++ src/components/HomeBg/index.tsx
@@ -1,5 +1,16 @@
+import Image from 'next/future/image'
+import bg_home from '../../assets/bg_home.svg'
+
 const HomeBg = () => (
-  <div className="bg-home bg-no-repeat bg-cover w-full h-[105vh] lg:h-[110vh] absolute top-0 -z-10" />
+  <div className="w-full h-[105vh] lg:h-[110vh] absolute top-0 -z-10">
+    <Image
+      src={bg_home}
+      className="w-full h-full object-cover"
+      alt="bg_home"
+      sizes="33vw"
+      quality={25}
+    />
+  </div>
 )
 
 export { HomeBg }
